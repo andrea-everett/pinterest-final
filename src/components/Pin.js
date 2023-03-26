96f46ecf9481bde3c4e21ed03b6a8c7e3e400272
andrea-everett <andrea.dawn.everett@gmail.com>
2023-03-26 14:10:06 -0400

    fix styling in modal

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -2,27 +2,35 @@ import React from "react";
 import "boxicons";
 
 function Pin(props) {
+  let gridRow = ''
   return (
-    <div>
-      <div className={`${props.pinDetails.pin_size}`}>
-      
-          <div className="pin_modal ">
-          
-            <div className="modal_head right-1 top-1 text-center w-3/12  ">
-              <div className="bg-red-600 top-2 right-20 hidden text-white rounded-full ">
-                Save
-              </div>
-            </div>
+    <a href={props.pinDetails.img_blob} target="_blank" rel="noreferrer" className='inline-block w-5/6 justify-center place-content-center rounded-lg'>
+      <img src={props.pinDetails.img_blob} alt='' altprop='' />
+      <div className='text-left pt-1 pb-5'>{props.pinDetails.title}</div>
+      <div className='text-left pt-1 pb-5'>{props.pinDetails.description}</div>
+    </a>
+  )
 
+  /* <div className={`${props.pinDetails.pin_size}`}> */
+
+  /* <div className="pin_modal">
+        <div className="pin_title_container">
+          <div className="modal_head right-1 top-1 text-center w-3/12  ">
+          </div>
+
+          <div className="pin_container bg-black h-5/6">
             <div className="modal_foot relative">
-              <div className="modal_foot_container">
-                <div className="destination">
-                  <div className="pin_mock_icon_container absolute bottom-1 w-3/12 bg-gray-200 rounded-md">
-                    <box-icon
-                      name="right-top-arrow-circle"
-                      className="pin_mock_icon p-1"
-                    ></box-icon>
-                  </div>
+              <div className="pin_image"></div>
+              <img
+                src={props.pinDetails.img_blob}
+                alt="pin_image rounded-full"
+              ></img>
+              <div className="destination text-left pt-1 pb-5">
+                <div className="pin_mock_icon_container absolute bottom-1 w-3/12 bg-gray-200 rounded-md text-left pt-1 pb-5">
+                  <box-icon
+                    name="right-top-arrow-circle"
+                    className="pin_mock_icon p-1"
+                  ></box-icon>
                 </div>
 
                 <div className="pin_mock_icon_container sm:absolute sm:bottom-1 sm:right-1 sm:bg-gray-200 rounded-lg">
@@ -31,29 +39,22 @@ function Pin(props) {
                     className="pin_mock_icon"
                   ></box-icon>
                 </div>
-                <div className="pin_image"></div>
-                <img
-                  src={props.pinDetails.img_blob}
-                  alt="pin_image rounded-full"
-                ></img>
               </div>
             </div>
           </div>
 
-          <div className="pin_title_container flex">
-            <div className="pin_title flex justify-end text-white pt-2 sm:text-black bottom text-lg">
+          <div className="details_container flex">
+            <div className="pin_title text-white pt-2 sm:text-white bottom text-lg">
               {props.pinDetails.title}
             </div>
-            {/* <box-icon name='dots-horizontal-rounded' className="pin_mock_icon items-end" color="white"></box-icon> */}
-          </div>
+            {/* <box-icon name='dots-horizontal-rounded' className="pin_mock_icon items-end" color="white"></box-icon> */
+  /* </div>
 
           <div className="pin_description text-white sm:text-black bottom text-sm">
             {props.pinDetails.description}
           </div>
-        </div>
-      </div>
-
-  );
+        </div >
+      </div > * /} */
 }
 
 export default Pin;
